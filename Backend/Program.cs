4c21dc771cb21b1b303a29610a55fc5b1ef87bea
Rizal <rizalmaulanaairlanggad4itb2024@gmail.com>
2026-02-07 16:19:52 +0700

    add Swagger for checking CRUD

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -4,12 +4,22 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
 );
 
 var app = builder.Build();
+
+// ✅ AKTIFKAN SWAGGER
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
 
 app.MapControllers();
 app.MapGet("/health", () => Results.Ok("healthy"));
